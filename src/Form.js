a746a00d672695ed27c2c6a06256601025950bf3
talum <tracy@flatironschool.com>
2019-11-06 14:28:54 -0500

    naive implementation of debounce

--- src/Form.js
+++ src/Form.js
@@ -5,6 +5,11 @@ import axios from 'axios';
 const SONG_URL = `/api/songs`
 
 class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.fetchSongs = this.debounce(this.makeApiRequest, this);
+  }
+
   state = {
     year: "",
     songs: [],
@@ -18,7 +23,11 @@ class Form extends Component {
 
   submit = () => {
     this.setState({ loading: true, songs: [], error: null });
-    axios.get(`${SONG_URL}/${this.state.year}`)
+    this.fetchSongs(this.state.year);
+  }
+
+  makeApiRequest = (year) => {
+    axios.get(`${SONG_URL}/${year}`)
       .then((resp) => {
         this.setState({
           songs: resp.data.songs,
@@ -35,16 +44,30 @@ class Form extends Component {
       });
   }
 
+  debounce = (func, context) => {
+    let lastTimeCalled = null;
+    return function(args) {
+      const tenSeconds = 10 * 1000;
+      if (lastTimeCalled && (Date.now() - lastTimeCalled > tenSeconds)) {
+        lastTimeCalled = Date.now();
+        func.call(context, args);
+      } else if (!lastTimeCalled) {
+        lastTimeCalled = Date.now();
+        func.call(context, args);
+      } else {
+        // no-op
+      }
+    }
+  }
+
   render() {
     return(
       <div className="Form">
-        { !this.state.loading &&
-          <div className="Form-inputs">
-            <p>Enter your graduation year to get a customized karaoke playlist</p>
-            <input type="text" onChange={this.handleChange} value={this.state.year} />
-            <input type="submit" onClick={this.submit} onSubmit={this.submit} />
-          </div>
-        }
+        <div className="Form-inputs">
+          <p>Enter your graduation year to get a customized karaoke playlist</p>
+          <input type="text" onChange={this.handleChange} value={this.state.year} />
+          <input type="submit" onClick={this.submit} onSubmit={this.submit} />
+        </div>
         <div className="Form-results">
           { this.state.loading  && 'One moment please' }
           { this.state.error && this.state.error }
